2ddc08d1404060f992ba36eb8c63ddef1c81636e
Niklas Lindenbaum <niklas.lindenbaum@t-online.de>
2022-09-15 16:56:35 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+
+
+
+const itsFriday = document.querySelector('#isFriday');
+const whichDay = document.createElement('p');
+const refreshSite = document.querySelector('img');
+const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const day = new Date();
+let today = weekday[day.getDay()];
+
+if (today === "Friday"){
+    itsFriday.textContent = "It's Friday 🎉";
+  } else {
+    whichDay.textContent = `Today is ${today}.`;
+    itsFriday.textContent = `Maybe tomorrow 🥲 `;
+    itsFriday.append(whichDay);
+  };
+
+refreshSite.addEventListener('click', () => {
+    refreshSite.style.width = "9%";
+    document.location.reload();
+})
